5210632888b36c47a33e2e451340fd20538fc03e
Traidepchetdoi <nguyenbicx2000@gmail.com>
2026-07-10 16:36:06 +0700

    Create BootReceiver.java

---
+++ app/src/main/java/com/omega/host/BootReceiver.java
@@ -0,0 +1,20 @@
+package com.omega.host;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) ||
+            "android.intent.action.QUICKBOOT_POWERON".equals(intent.getAction()) ||
+            "com.htc.intent.action.QUICKBOOT_POWERON".equals(intent.getAction())) {
+            
+            // Tự khởi động MainActivity khi máy bật
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+        }
+    }
+}
