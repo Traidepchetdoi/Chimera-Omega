309c1cf74a941e36e19182b7ec7eb3a8da95503d
Traidepchetdoi <nguyenbicx2000@gmail.com>
2026-07-10 19:48:11 +0700

    Update VoidEngine.cpp

--- app/src/main/cpp/VoidEngine.cpp
+++ app/src/main/cpp/VoidEngine.cpp
@@ -5,8 +5,8 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "OMEGA_CPP", __VA_ARGS__)
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_omega_host_VoidEngine_getCoreStatus(JNIEnv* env, jobject) {
-    LOGI("C++ Core Engaged. SWAR Engine Online.");
+Java_com_omega_host_MainActivity_getCoreStatus(JNIEnv* env, jobject) {
+    __android_log_print(ANDROID_LOG_INFO, "OMEGA_CPP", "C++ Core Engaged. SWAR Engine Online.");
     std::string status = "[OMEGA C++] Lõi SWAR đã kích hoạt. Tàng hình tuyệt đối.";
     return env->NewStringUTF(status.c_str());
 }
